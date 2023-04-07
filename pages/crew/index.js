abbc16492cf4763786caa42c23c177b9e71f0791
Weiwei <w1216lin@gmail.com>
2023-04-06 21:21:17 -0500

    fix: load scripts

--- pages/crew/index.js
+++ pages/crew/index.js
@@ -1,19 +1,20 @@
-import $ from 'jquery';
+import $ from "jquery";
 import data from "../../data.json";
 import { activeSideBar, defaultSelected } from "../../utils/funcs";
-import { crewPic as pic } from '../../utils/pictures';
+import { crewPic as pic } from "../../utils/pictures";
 
 activeSideBar();
 const { crew } = data;
 
 $(".crew-list button").each((index, elem) => {
-  $(elem).on('click', (e) => {
+  $(elem).on("click", (e) => {
     defaultSelected(e.target);
-    $('#crew-role').text(crew[index].role);
-    $('#crew-bio').text(crew[index].bio);
-    $('#crew-name').text(crew[index].name);
-    $('#crew-picture source').attr("srcset", pic[index].png);
-    $('#crew-picture img').attr("src", pic[index].webp).attr('alt', crew[index].role);
-  })
-})
-
+    $("#crew-role").text(crew[index].role);
+    $("#crew-bio").text(crew[index].bio);
+    $("#crew-name").text(crew[index].name);
+    $("#crew-picture source").attr("srcset", pic[index].png);
+    $("#crew-picture img")
+      .attr("src", pic[index].webp)
+      .attr("alt", crew[index].role);
+  });
+});
